f691347cc404a16c1014e574a777254080b9c93d
Have Samuel <samiehave9@gmail.com>
2023-10-12 15:20:44 +0300

    Appending the row to the board

--- app.js
+++ app.js
@@ -1,223 +1,223 @@
-/* eslint-disable max-len */
-// categories is the main data structure for the app; it looks like this:
+// /* eslint-disable max-len */
+// // categories is the main data structure for the app; it looks like this:
 
-const categories = [
-  {
-    title: 'American Revolution',
-    id: 150,
-    clues: [
-      { question: 'Nickname of the British from the color of their clothing?', answer: 'The Redcoats', showing: null },
-      { question: '1768 American war tune which was set to a 1500s melody', answer: 'Yankee Doodle', showing: null },
-      { question: 'Brigadier Gen. O Hara went through the final surrender in place of this English commander', answer: 'Cornwallis', showing: null },
-      { question: 'The 2 foreign countries which fought on our side', answer: 'France \u0026 Spain', showing: null },
-      { question: 'General Howe faced him in Brandywine, Germantown, \u0026 Long Island battles', answer: 'George Washington', showing: null },
-    ],
-  },
-  {
-    title: 'World Geography',
-    id: 121,
-    clues: [
-      { question: 'Capital of Sri Lanka, it sounds like Peter Falk\'s been there', answer: 'Colombo', showing: null },
-      { question: 'Continent Greenland is considered part of..', answer: 'North America', showing: null },
-      { question: 'As Cape Horn is to South America, this is to Africa', answer: 'The Cape of Good Hope', showing: null },
-      { question: 'Closest foreign country to Fairbanks, Alaska', answer: 'Canada', showing: null },
-      { question: 'The Gulf of Aqaba separates these 2 Mid-East peninsulas', answer: 'The Arabian Peninsula \u0026 the Sinai Peninsula', showing: null },
-    ],
-  },
-  {
-    title: 'Winter Sports',
-    id: 50,
-    clues: [
-      { question: 'Motorized sledding', answer: 'Snowmobiling', showing: null },
-      { question: 'To do this, you put 2 \\"baskets\\" on your feet \u0026 take three giant steps', answer: 'Snowshoe', showing: null },
-      { question: 'The diagonal stride is the basic move in this popular form of Nordic skiing', answer: 'Cross Country', showing: null },
-      { question: 'In the 18th C., iceboating originated on the frozen canals of this north European country', answer: 'Holland (the Netherlands)', showing: null },
-      { question: 'They were 1st held at Chamonix, France in 1924', answer: 'The Winter Olympics', showing: null },
-    ],
-  },
-  {
-    title: '1933',
-    id: 40,
-    clues: [
-      { question: 'You never saw FDR smoking a cigarette without one of these', answer: 'Cigarette Holder', showing: null },
-      { question: 'He came to the U.S. in 1933 \u0026 helped found the School of American Ballet', answer: 'George Balanchine', showing: null },
-      { question: 'In Berlin, in May, this was done to works of Freud, Einstein and Thomas Mann, among others', answer: 'Burning', showing: null },
-      { question: 'Ratified in 1933, the 21st Amendment repealed Prohibition, put into effect by this amendment', answer: 'The 18th Amendment', showing: null },
-      { question: 'On February 15 this president-elect survived an assassination attempt by Giuseppe Zangara', answer: 'Franklin D. Roosevelt', showing: null },
-    ],
-  },
-  {
-    title: 'Awards',
-    id: 160,
-    clues: [
-      { question: 'British decoration named for former queen', answer: 'Victoria Cross', showing: null },
-      { question: 'In the 80s Mario Lemieux \u0026 this man won every MVP award in the NHL', answer: 'Wayne Gretzky', showing: null },
-      { question: 'Since 32 the U.S. Badge of Military Merit has had this colorful name', answer: 'The Purple Heart', showing: null },
-      { question: 'The Grammy Album of the Year for 1960 was this comic\\s \\"Button Down Mind\\"', answer: 'Bob Newhart', showing: null },
-      { question: '26 years after writing it for the stage, he won Oscar for the screenplay \\"Pygmalion\\"', answer: 'George Bernard Shaw', showing: null },
-    ],
-  },
-  {
-    title: 'The Bible',
-    id: 105,
-    clues: [
-      { question: 'The 1st book of the Bible', answer: 'Genesis', showing: null },
-      { question: 'The 2nd book of the Bible', answer: 'Exodus', showing: null },
-      { question: 'The 3rd book of the Bible', answer: 'Leviticus', showing: null },
-      { question: 'The 4th book of the Bible', answer: 'Numbers', showing: null },
-      { question: 'The 5th book of the Bible', answer: 'Deuteronomy', showing: null },
-    ],
-  },
-  // {
-  //   title: 'The 50 States',
-  //   id: 10,
-  //   clues: [
-  //     { question: 'The 1st state', answer: 'Delaware', showing: null },
-  //     { question: 'The 2nd state', answer: 'Pennsylvania', showing: null },
-  //     { question: 'The 3rd state', answer: 'New Jersey', showing: null },
-  //     { question: 'The 4th state', answer: 'Georgia', showing: null },
-  //     { question: 'The 5th state', answer: 'Connecticut', showing: null },
-  //   ],
-  // },
-];
+// const categories = [
+//   {
+//     title: 'American Revolution',
+//     id: 150,
+//     clues: [
+//       { question: 'Nickname of the British from the color of their clothing?', answer: 'The Redcoats', showing: null },
+//       { question: '1768 American war tune which was set to a 1500s melody', answer: 'Yankee Doodle', showing: null },
+//       { question: 'Brigadier Gen. O Hara went through the final surrender in place of this English commander', answer: 'Cornwallis', showing: null },
+//       { question: 'The 2 foreign countries which fought on our side', answer: 'France \u0026 Spain', showing: null },
+//       { question: 'General Howe faced him in Brandywine, Germantown, \u0026 Long Island battles', answer: 'George Washington', showing: null },
+//     ],
+//   },
+//   {
+//     title: 'World Geography',
+//     id: 121,
+//     clues: [
+//       { question: 'Capital of Sri Lanka, it sounds like Peter Falk\'s been there', answer: 'Colombo', showing: null },
+//       { question: 'Continent Greenland is considered part of..', answer: 'North America', showing: null },
+//       { question: 'As Cape Horn is to South America, this is to Africa', answer: 'The Cape of Good Hope', showing: null },
+//       { question: 'Closest foreign country to Fairbanks, Alaska', answer: 'Canada', showing: null },
+//       { question: 'The Gulf of Aqaba separates these 2 Mid-East peninsulas', answer: 'The Arabian Peninsula \u0026 the Sinai Peninsula', showing: null },
+//     ],
+//   },
+//   {
+//     title: 'Winter Sports',
+//     id: 50,
+//     clues: [
+//       { question: 'Motorized sledding', answer: 'Snowmobiling', showing: null },
+//       { question: 'To do this, you put 2 \\"baskets\\" on your feet \u0026 take three giant steps', answer: 'Snowshoe', showing: null },
+//       { question: 'The diagonal stride is the basic move in this popular form of Nordic skiing', answer: 'Cross Country', showing: null },
+//       { question: 'In the 18th C., iceboating originated on the frozen canals of this north European country', answer: 'Holland (the Netherlands)', showing: null },
+//       { question: 'They were 1st held at Chamonix, France in 1924', answer: 'The Winter Olympics', showing: null },
+//     ],
+//   },
+//   {
+//     title: '1933',
+//     id: 40,
+//     clues: [
+//       { question: 'You never saw FDR smoking a cigarette without one of these', answer: 'Cigarette Holder', showing: null },
+//       { question: 'He came to the U.S. in 1933 \u0026 helped found the School of American Ballet', answer: 'George Balanchine', showing: null },
+//       { question: 'In Berlin, in May, this was done to works of Freud, Einstein and Thomas Mann, among others', answer: 'Burning', showing: null },
+//       { question: 'Ratified in 1933, the 21st Amendment repealed Prohibition, put into effect by this amendment', answer: 'The 18th Amendment', showing: null },
+//       { question: 'On February 15 this president-elect survived an assassination attempt by Giuseppe Zangara', answer: 'Franklin D. Roosevelt', showing: null },
+//     ],
+//   },
+//   {
+//     title: 'Awards',
+//     id: 160,
+//     clues: [
+//       { question: 'British decoration named for former queen', answer: 'Victoria Cross', showing: null },
+//       { question: 'In the 80s Mario Lemieux \u0026 this man won every MVP award in the NHL', answer: 'Wayne Gretzky', showing: null },
+//       { question: 'Since 32 the U.S. Badge of Military Merit has had this colorful name', answer: 'The Purple Heart', showing: null },
+//       { question: 'The Grammy Album of the Year for 1960 was this comic\\s \\"Button Down Mind\\"', answer: 'Bob Newhart', showing: null },
+//       { question: '26 years after writing it for the stage, he won Oscar for the screenplay \\"Pygmalion\\"', answer: 'George Bernard Shaw', showing: null },
+//     ],
+//   },
+//   {
+//     title: 'The Bible',
+//     id: 105,
+//     clues: [
+//       { question: 'The 1st book of the Bible', answer: 'Genesis', showing: null },
+//       { question: 'The 2nd book of the Bible', answer: 'Exodus', showing: null },
+//       { question: 'The 3rd book of the Bible', answer: 'Leviticus', showing: null },
+//       { question: 'The 4th book of the Bible', answer: 'Numbers', showing: null },
+//       { question: 'The 5th book of the Bible', answer: 'Deuteronomy', showing: null },
+//     ],
+//   },
+//   // {
+//   //   title: 'The 50 States',
+//   //   id: 10,
+//   //   clues: [
+//   //     { question: 'The 1st state', answer: 'Delaware', showing: null },
+//   //     { question: 'The 2nd state', answer: 'Pennsylvania', showing: null },
+//   //     { question: 'The 3rd state', answer: 'New Jersey', showing: null },
+//   //     { question: 'The 4th state', answer: 'Georgia', showing: null },
+//   //     { question: 'The 5th state', answer: 'Connecticut', showing: null },
+//   //   ],
+//   // },
+// ];
 
-// let categories = [];
+// // let categories = [];
 
-/** Get NUM_CATEGORIES random category from API.
- *
- * Returns array of category ids
- */
+// /** Get NUM_CATEGORIES random category from API.
+//  *
+//  * Returns array of category ids
+//  */
 
-// Reset Button
-// const gameBtn = document.getElementsByName('button');
+// // Reset Button
+// // const gameBtn = document.getElementsByName('button');
 
-// gameBtn.addEventListener('click', () => {
+// // gameBtn.addEventListener('click', () => {
 
-// });
+// // });
 
-async function getCategoryIds() {
-  // Fetching the data from the API
-  const response = await axios.get('https://jservice.io/api/clues', {
-    params: {
-      question: 'question', answer: 'answer',
-    },
-  });
-  // Getting the data from the response
-  const { data } = response;
-  console.log(data);
-  // Getting the category ids from the data
-  const categoryIds = data.map((category) => category.category_id);
-  console.log(categoryIds);
-  console.log(categories);
+// async function getCategoryIds() {
+//   // Fetching the data from the API
+//   const response = await axios.get('https://jservice.io/api/clues', {
+//     params: {
+//       question: 'question', answer: 'answer',
+//     },
+//   });
+//   // Getting the data from the response
+//   const { data } = response;
+//   console.log(data);
+//   // Getting the category ids from the data
+//   const categoryIds = data.map((category) => category.category_id);
+//   console.log(categoryIds);
+//   console.log(categories);
 
-  // When the showing variable is empty, the data should show null, when it has the answer,
-  // it should show answer, and when it has question it should show question in the data attribute.
-}
+//   // When the showing variable is empty, the data should show null, when it has the answer,
+//   it should show answer, and when it has question it should show question in the data attribute.
+// }
 
-getCategoryIds();
+// getCategoryIds();
 
-function getCategory(catId) {
-}
+// function getCategory(catId) {
+// }
 
-// Fill the HTML table#jeopardy with the categories & cells for questions.
-const jeopardy = document.querySelector('#jeopardy');
-jeopardy.classList = 'whole-board';
+// // Fill the HTML table#jeopardy with the categories & cells for questions.
+// const jeopardy = document.querySelector('#jeopardy');
+// jeopardy.classList = 'whole-board';
 
-// The first row should be filled with <th> elements, one for each category
+// // The first row should be filled with <th> elements, one for each category
 
-function addCategory(category) {
-  // const thead = document.createElement('thead');
-  // const tbody = document.createElement('tbody');
-  const thead = document.querySelector('#head');
-  thead.classList = 'show-up';
-  const tbody = document.querySelector('#body');
-  // tbody.classList = 'body-stuf';
+// function addCategory(category) {
+//   // const thead = document.createElement('thead');
+//   // const tbody = document.createElement('tbody');
+//   const thead = document.querySelector('#head');
+//   thead.classList = 'show-up';
+//   const tbody = document.querySelector('#body');
+//   // tbody.classList = 'body-stuf';
 
-  const trHead = document.createElement('tr');
-  const th = document.createElement('th');
-  th.classList = 'head-stuf';
+//   const trHead = document.createElement('tr');
+//   const th = document.createElement('th');
+//   th.classList = 'head-stuf';
 
-  th.innerHTML = category.title;
-  trHead.appendChild(th);
-  console.log(trHead);
-  thead.appendChild(trHead);
-  // tbody.appendChild(tr1);
-  jeopardy.append(thead);
+//   th.innerHTML = category.title;
+//   trHead.appendChild(th);
+//   console.log(trHead);
+//   thead.appendChild(trHead);
+//   // tbody.appendChild(tr1);
+//   jeopardy.append(thead);
 
-  category.clues.forEach((clue) => {
-    const trBody = document.createElement('tr');
-    const td = document.createElement('td');
-    trBody.classList = 'card';
-    td.innerText = '#';
-    // td.classList = 'show-me';
-    trBody.appendChild(td);
-    tbody.appendChild(trBody);
-    jeopardy.append(tbody);
-    // Adding attributes to each td
-    // Each td should have the clue question, answer, and showing status
-    // stored as data attributes on the HTML element.
-    td.setAttribute('data-question', clue.question);
-    td.setAttribute('data-answer', clue.answer);
-    td.setAttribute('data-showing', clue.showing);
-    // Adding event listener to each td
-    // When clicked, the td should show either the question or the answer.
-    td.addEventListener('click', (e) => {
-      const card = e.target;
-      const question = card.getAttribute('data-question');
-      const answer = card.getAttribute('data-answer');
-      const showing = card.getAttribute('data-showing');
+//   category.clues.forEach((clue) => {
+//     const trBody = document.createElement('tr');
+//     const td = document.createElement('td');
+//     trBody.classList = 'card';
+//     td.innerText = '#';
+//     // td.classList = 'show-me';
+//     trBody.appendChild(td);
+//     tbody.appendChild(trBody);
+//     jeopardy.append(tbody);
+//     // Adding attributes to each td
+//     // Each td should have the clue question, answer, and showing status
+//     // stored as data attributes on the HTML element.
+//     td.setAttribute('data-question', clue.question);
+//     td.setAttribute('data-answer', clue.answer);
+//     td.setAttribute('data-showing', clue.showing);
+//     // Adding event listener to each td
+//     // When clicked, the td should show either the question or the answer.
+//     td.addEventListener('click', (e) => {
+//       const card = e.target;
+//       const question = card.getAttribute('data-question');
+//       const answer = card.getAttribute('data-answer');
+//       const showing = card.getAttribute('data-showing');
 
-      if (showing === 'null') {
-        card.innerText = question;
-        card.setAttribute('data-showing', 'question');
-      } else if (showing === 'question') {
-        card.innerText = answer;
-        card.setAttribute('data-showing', 'answer');
-      }
-    });
-  });
-}
+//       if (showing === 'null') {
+//         card.innerText = question;
+//         card.setAttribute('data-showing', 'question');
+//       } else if (showing === 'question') {
+//         card.innerText = answer;
+//         card.setAttribute('data-showing', 'answer');
+//       }
+//     });
+//   });
+// }
 
-categories.forEach((category) => addCategory(category));
+// categories.forEach((category) => addCategory(category));
 
-// When one card is clicked, all other cards in that category should be disabled.
+// // When one card is clicked, all other cards in that category should be disabled.
 
-async function fillTable() {
-}
+// async function fillTable() {
+// }
 
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
+// /** Handle clicking on a clue: show the question or answer.
+//  *
+//  * Uses .showing property on clue to determine what to show:
+//  * - if currently null, show question & set .showing to "question"
+//  * - if currently "question", show answer & set .showing to "answer"
+//  * - if currently "answer", ignore click
+//  * */
 
-function handleClick(evt) {
-}
+// function handleClick(evt) {
+// }
 
-/** Wipe the current Jeopardy board, show the loading spinner,
- * and update the button used to fetch data.
- */
+// /** Wipe the current Jeopardy board, show the loading spinner,
+//  * and update the button used to fetch data.
+//  */
 
-function showLoadingView() {
+// function showLoadingView() {
 
-}
+// }
 
-/** Remove the loading spinner and update the button used to fetch data. */
+// /** Remove the loading spinner and update the button used to fetch data. */
 
-function hideLoadingView() {
-}
+// function hideLoadingView() {
+// }
 
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
+// /** Start game:
+//  *
+//  * - get random category Ids
+//  * - get data for each category
+//  * - create HTML table
+//  * */
 
-async function setupAndStart() {
-}
+// async function setupAndStart() {
+// }
 
 /** On click of start / restart button, set up game. */
 
@@ -229,18 +229,30 @@ async function setupAndStart() {
 
 // Initialize the game board on the page load
 function initBoord() {
-  let board = document.getElementById('clue-board');
+  const board = document.getElementById('clue-board');
+
+  function getClue() {
+    console.log('Have a nice day!!');
+  }
 
   // Generate 5 rows, then place 6 boxes in each row
-  for (let i = 0; i < 5; i++) {
-    let row = document.createElement('div');
-    let boxValue = '#';
+  for (let i = 0; i < 5; i += 1) {
+    const row = document.createElement('div');
+    const boxValue = '#';
     row.className = 'clue-row';
 
-    for (let j = 0; j < 6; j++) {
+    for (let j = 0; j < 6; j += 1) {
       // creating six 'divs' inside the row (Boxes)
       const box = document.createElement('div');
       box.className = 'clue-box';
+      // Box inner text
+      box.textContent = boxValue;
+      box.addEventListener('click', getClue, false);
+      row.appendChild(box);
     }
+    // Appending the Row to the board
+    board.appendChild(row);
   }
 }
+
+initBoord();
